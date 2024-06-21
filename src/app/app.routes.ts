fc138600b968c84a6f10bee4428737b300e8b737
Joaquin Ortega <joaquin.blanco@bootcamp-upgrade.com>
2024-06-21 11:08:51 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { TrabajosComponent } from './pages/trabajos/trabajos.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {
+    path: '',
+    component: MainComponent
+   },
+   {
+    path: 'projects',
+    component: TrabajosComponent
+   },
+   {
+    path: 'skills',
+    component: SkillsComponent
+   },
+   {
+    path: 'about',
+    component: AboutComponent
+   },
+   {
+    path: 'contact',
+    component: ContactComponent
+   }
+];
